85da050be602a0d7d8d16cc709e2571a9dfe2d0f
Nevo Cohen <nevocohen111@gmail.com>
2022-05-22 13:54:51 +0300

    git commit -m "Coffee-Machine"

--- src/com/nevo/Main.java
+++ src/com/nevo/Main.java
@@ -4,10 +4,7 @@ import com.nevo.enums.*;
 
 public class Main {
     public static void main(String[] args) {
-        Coffee coffee = new Coffee.CoffeeBuilder(ECoffeeDrinkType.AMERICANO)
-                .setWithSize(ECoffeeCupSize.SMALL)
-                .setWithDrinkLevel(ECoffeeDrinkLevel.HEAVY)
-                .setWithMachineType(ECoffeeMachineType.SEMI_MACHINE).build();
+        Coffee coffee = new Coffee.CoffeeBuilder(ECoffeeDrinkType.AMERICANO).build();
         coffee.makeCoffee();
     }
 
